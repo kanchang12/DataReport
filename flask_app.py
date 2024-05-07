5065ad686fbc3cbdac4fec68663d76244906ea7b
kanchang12 <32091338+kanchang12@users.noreply.github.com>
2024-05-07 15:45:52 +0100

    Add files via upload

--- flask_app.py
+++ flask_app.py
@@ -47,5 +47,4 @@ def process_data():
     return render_template('analysis.html', analysis_result=analysis_result)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
