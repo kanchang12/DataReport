b75bd47c2e223eab20066fc3f1d095f7b31599ab
kanchang12 <32091338+kanchang12@users.noreply.github.com>
2024-05-07 11:24:20 +0100

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -47,4 +47,5 @@ def process_data():
     return render_template('analysis.html', analysis_result=analysis_result)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
